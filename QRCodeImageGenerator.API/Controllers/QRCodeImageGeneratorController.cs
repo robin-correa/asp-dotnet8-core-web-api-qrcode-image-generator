3844f4c69b2f0c51564701ed332753f8bf558ca3
robin.correa <robin.correa21@gmail.com>
2024-06-03 23:00:53 +0800

    Refactor to use DTO.

--- QRCodeImageGenerator.API/Controllers/QRCodeImageGeneratorController.cs
+++ QRCodeImageGenerator.API/Controllers/QRCodeImageGeneratorController.cs
@@ -3,6 +3,8 @@ using System.Drawing.Imaging;
 
 using Microsoft.AspNetCore.Mvc;
 
+using QRCodeImageGenerator.API.DataTransferObjects;
+
 using QRCoder;
 
 namespace QRCodeImageGenerator.API.Controllers
@@ -11,42 +13,52 @@ namespace QRCodeImageGenerator.API.Controllers
     [ApiController]
     public class QRCodeImageGeneratorController : ControllerBase
     {
-        [HttpGet]
+        private static readonly string LogoPath = "./Images/logo.png";
+
+        [HttpPost]
         [Route("generate")]
-        public IActionResult Generate(string text = "Robin Correa", string logoPath = "./Images/logo.png")
+        public IActionResult Generate([FromBody] GenerateRequestDTO request)
         {
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
-
-            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
-            byte[] qrCodeImage = qrCode.GetGraphic(20);
-
-            // Load the logo image
-            using (var ms = new MemoryStream(qrCodeImage))
-            using (var qrCodeBitmap = new Bitmap(ms))
+            try
             {
-                // Resize the QR code to 300x300
-                Bitmap resizedQrCodeBitmap = new Bitmap(qrCodeBitmap, new Size(300, 300));
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(request.Text, QRCodeGenerator.ECCLevel.Q);
 
-                var logo = Image.FromFile(logoPath);
+                PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+                byte[] qrCodeImage = qrCode.GetGraphic(20);
 
-                // Calculate logo size and position
-                int logoSize = resizedQrCodeBitmap.Height / 5;
-                var logoPosition = new Point((resizedQrCodeBitmap.Width - logoSize) / 2, (resizedQrCodeBitmap.Height - logoSize) / 2);
-
-                // Add logo to QR code
-                using (var graphics = Graphics.FromImage(resizedQrCodeBitmap))
+                // Load the logo image
+                using (var ms = new MemoryStream(qrCodeImage))
+                using (var qrCodeBitmap = new Bitmap(ms))
                 {
-                    graphics.DrawImage(logo, new Rectangle(logoPosition, new Size(logoSize, logoSize)));
-                }
+                    // Resize the QR code to specified dimensions
+                    Bitmap resizedQrCodeBitmap = new Bitmap(qrCodeBitmap, new Size(request.Width, request.Height));
 
-                // Convert the final image to a base64 string
-                using (var stream = new MemoryStream())
-                {
-                    resizedQrCodeBitmap.Save(stream, ImageFormat.Png);
-                    var base64String = Convert.ToBase64String(stream.ToArray());
-                    return Ok(new { base64Image = base64String });
+                    // Load the main logo image
+                    var logo = Image.FromFile(LogoPath);
+
+                    // Calculate logo size and position
+                    int logoSize = resizedQrCodeBitmap.Height / 5;
+                    var logoPosition = new Point((resizedQrCodeBitmap.Width - logoSize) / 2, (resizedQrCodeBitmap.Height - logoSize) / 2);
+
+                    // Add main logo to QR code
+                    using (var graphics = Graphics.FromImage(resizedQrCodeBitmap))
+                    {
+                        graphics.DrawImage(logo, new Rectangle(logoPosition, new Size(logoSize, logoSize)));
+                    }
+
+                    // Convert the final image to a base64 string
+                    using (var stream = new MemoryStream())
+                    {
+                        resizedQrCodeBitmap.Save(stream, ImageFormat.Png);
+                        var base64String = Convert.ToBase64String(stream.ToArray());
+                        return Ok(new { base64Image = base64String });
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
     }
---
+++ QRCodeImageGenerator.API/DataTransferObjects/GenerateRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace QRCodeImageGenerator.API.DataTransferObjects
+{
+    public class GenerateRequestDTO
+    {
+        public string Text { get; set; }
+        public string QRLogoColorHex { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
